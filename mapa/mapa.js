b2308896966662a372045b39ffc321d4dd16b3cb
GabrielBTostes1 <20251ac008007@estudantes.ifpr.edu.br>
2026-05-05 14:40:57 -0300

    adicionei mais coisas no mapa

--- mapa/mapa.js
+++ mapa/mapa.js
@@ -1,64 +1,73 @@
-  const meuMapa = L.map('mapa').setView([-24.4183, -53.5210], 14);
+const meuMapa = L.map('mapa').setView([-24.4183, -53.5210], 14);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; OpenStreetMap contributors'
-        }).addTo(meuMapa);
-
-  
-        const grupoMobilidade = L.layerGroup();
-        const grupoTurismo = L.layerGroup();
-        const grupoGastronomia = L.layerGroup();
-        const grupoPostos = L.layerGroup();
-
-      
-        L.marker([-24.4150, -53.5200]).bindPopup("<b>Rodoviária</b>").addTo(grupoMobilidade);
-        L.marker([-24.4200, -53.5180]).bindPopup("<b>Ponto de Táxi</b>").addTo(grupoMobilidade);
-
-     
-        L.marker([-24.4068551, -53.5205348]).bindPopup("<b>Horto Florestal</b>").addTo(grupoTurismo);
-        L.marker([-24.4110, -53.5300]).bindPopup("<b>Praça Central</b>").addTo(grupoTurismo);
-
-       
-        L.marker([-24.4180, -53.5220]).bindPopup("<b>Restaurante da Praça</b>").addTo(grupoGastronomia);
-        L.marker([-24.4195, -53.5190]).bindPopup("<b>Pizzaria Boa Forma</b>").addTo(grupoGastronomia);
-
-       
-        L.marker([-24.4021466, -53.5130465]).bindPopup("<b>Posto Ipiranga- Avaliaçao: ⭐⭐⭐⭐⭐ </b>").addTo(grupoPostos);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(meuMapa);
 
 
-    
-        const todasCategorias = {
-            "mobilidade": grupoMobilidade,
-            "turismo": grupoTurismo,
-            "gastronomia": grupoGastronomia,
-            "postos": grupoPostos
-        };
+const grupoMobilidade = L.layerGroup();
+const grupoTurismo = L.layerGroup();
+const grupoGastronomia = L.layerGroup();
+const grupoPostos = L.layerGroup();
 
-     
-        grupoMobilidade.addTo(meuMapa);
+//paradas de onibus
+L.marker([-24.404883, -53.516982]).bindPopup("<b>Rodoviária</b>").addTo(grupoMobilidade);
+L.marker([-24.4200, -53.5180]).bindPopup("<b>Ponto Teste</b>").addTo(grupoMobilidade);
 
-        
-        const botoes = document.querySelectorAll('.btn-filtro');
+//Turismo
+L.marker([-24.406691, -53.514187]).bindPopup("<b>Praça dos Trabalhadores</b>").addTo(grupoTurismo);
+L.marker([-24.4068551, -53.5205348]).bindPopup("<b>Horto Florestal</b>").addTo(grupoTurismo);
+L.marker([-24.414238, -53.518487]).bindPopup("<b>Praça Paris</b>").addTo(grupoTurismo);
+L.marker([-24.403970, -53.509547]).bindPopup("<b>Teatro Municipal</b>").addTo(grupoTurismo);
+L.marker([-24.397700, -53.511709]).bindPopup("<b>Praça que eu e bruno fica depois do rodizio labella</b>").addTo(grupoTurismo);
+L.marker([-24.391224, -53.514691]).bindPopup("<b>Ginásio de Esportes Tancredo Neves</b>").addTo(grupoTurismo);
+L.marker([-24.407695, -53.533094]).bindPopup("<b>Centro de Evenetos</b>").addTo(grupoTurismo);
+L.marker([-24.397934, -53.502696]).bindPopup("<b>Praça do Panorama</b>").addTo(grupoTurismo);
 
-    
-        botoes.forEach(function(botao) {
-            botao.addEventListener('click', function() {
-                
-             
-                botoes.forEach(b => b.classList.remove('ativo'));
 
-                this.classList.add('ativo');
 
-                
-                for (var key in todasCategorias) {
-                    meuMapa.removeLayer(todasCategorias[key]);
-                }
+//gastronomia
+L.marker([-24.4180, -53.5220]).bindPopup("<b>Restaurante da Praça(teste)</b>").addTo(grupoGastronomia);
+L.marker([-24.4195, -53.5190]).bindPopup("<b>Pizzaria Boa Forma(teste)</b>").addTo(grupoGastronomia);
 
-            
-                var categoriaClicada = this.getAttribute('data-categoria');
+//Postos
+L.marker([-24.4021466, -53.5130465]).bindPopup("<b>Posto Ipiranga</b>").addTo(grupoPostos);
+L.marker([-24.403911, -53.535272]).bindPopup("<b>Posto Colombo</b>").addTo(grupoPostos)
 
-               
-                todasCategorias[categoriaClicada].addTo(meuMapa);
-            });
-        });
+
+
+const todasCategorias = {
+    "mobilidade": grupoMobilidade,
+    "turismo": grupoTurismo,
+    "gastronomia": grupoGastronomia,
+    "postos": grupoPostos
+};
+
+
+grupoMobilidade.addTo(meuMapa);
+
+
+const botoes = document.querySelectorAll('.btn-filtro');
+
+
+botoes.forEach(function (botao) {
+    botao.addEventListener('click', function () {
+
+
+        botoes.forEach(b => b.classList.remove('ativo'));
+
+        this.classList.add('ativo');
+
+
+        for (var key in todasCategorias) {
+            meuMapa.removeLayer(todasCategorias[key]);
+        }
+
+
+        var categoriaClicada = this.getAttribute('data-categoria');
+
+
+        todasCategorias[categoriaClicada].addTo(meuMapa);
+    });
+});
