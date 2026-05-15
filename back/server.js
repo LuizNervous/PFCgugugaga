3edbe118b3fe0f7c33f65799a05266393f045193
LuizNervous <luiz.gu.neves@gmail.com>
2026-05-15 18:36:59 -0300

    esta tudo funcionando, falta ficar bonito

---
+++ back/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express=require('express');
+const cors=require('cors');
+const mysql=require('mysql2');
+
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME
+});
+db.connect((err) => {
+    if (err) {
+        console.error('Erro de conexão:', err.message);
+        return;
+    }
+    console.log('Conectado ao banco de dados com sucesso!');
+});
+
+app.get('/api/pontos',(req,res)=>{
+    const query=`
+    SELECT p.*, c.nome AS categoria_nome
+    FROM pontos p
+    JOIN categorias c ON p.id_categoria = c.id`;
+    db.query(query,(erro, results)=>{
+        if (erro) {
+            return res.status(500).json(erro);  
+        };
+        res.json(results);
+    });
+});
+
+app.listen(3000, ()=> console.log("servidor rodando na porta 3000"));
--- mapa/mapa.js
+++ mapa/mapa.js
@@ -11,68 +11,83 @@ const grupoTurismo = L.layerGroup();
 const grupoGastronomia = L.layerGroup();
 const grupoPostos = L.layerGroup();
 const grupoHospitais = L.layerGroup();
-const grupoPostosSaude=L.layerGroup();
+const grupoPostosSaude = L.layerGroup();
 const grupoFarmacia = L.layerGroup();
-//paradas de onibus
-L.marker([-24.404883, -53.516982]).bindPopup("<b>Rodoviária</b>").addTo(grupoMobilidade);
-L.marker([-24.4200, -53.5180]).bindPopup("<b>Ponto Teste</b>").addTo(grupoMobilidade);
-
-//Turismo
-L.marker([-24.406691, -53.514187]).bindPopup("<b>Praça dos Trabalhadores</b>").addTo(grupoTurismo);
-L.marker([-24.4068551, -53.5205348]).bindPopup("<b>Horto Florestal</b>").addTo(grupoTurismo);
-L.marker([-24.414238, -53.518487]).bindPopup("<b>Praça Paris</b>").addTo(grupoTurismo);
-L.marker([-24.403970, -53.509547]).bindPopup("<b>Teatro Municipal</b>").addTo(grupoTurismo);
-L.marker([-24.397700, -53.511709]).bindPopup("<b>Praça das Américas</b>").addTo(grupoTurismo);
-L.marker([-24.391224, -53.514691]).bindPopup("<b>Ginásio de Esportes Tancredo Neves</b>").addTo(grupoTurismo);
-L.marker([-24.407695, -53.533094]).bindPopup("<b>Centro de Evenetos</b>").addTo(grupoTurismo);
-L.marker([-24.397934, -53.502696]).bindPopup("<b>Praça do Panorama</b>").addTo(grupoTurismo);
-
-
-
-//gastronomia
-L.marker([-24.4180, -53.5220]).bindPopup("<b>Restaurante da Praça(teste)</b>").addTo(grupoGastronomia);
-L.marker([-24.4195, -53.5190]).bindPopup("<b>Pizzaria Boa Forma(teste)</b>").addTo(grupoGastronomia);
-
-//Postos
-L.marker([-24.4021466, -53.5130465]).bindPopup("<b>Posto Ipiranga</b>").addTo(grupoPostos);
-L.marker([-24.403911, -53.535272]).bindPopup("<b>Posto Colombo</b>").addTo(grupoPostos)
-
-
 
 const todasCategorias = {
     "mobilidade": grupoMobilidade,
     "turismo": grupoTurismo,
     "gastronomia": grupoGastronomia,
-    "postos": grupoPostos,
-    "hospitais":grupoHospitais,
-    "postossaude":grupoPostosSaude,
-    "farmacias":grupoFarmacia
+    "posto": grupoPostos,
+    "hospital": grupoHospitais,
+    "posto de saúde": grupoPostosSaude,
+    "farmacia": grupoFarmacia
 };
+grupoMobilidade.addTo(meuMapa)
 
+async function carregarPontos() {
+    try {
+        const resposta = await fetch("http://localhost:3000/api/pontos");
+        const pontos = await resposta.json();
 
-grupoMobilidade.addTo(meuMapa);
+        const divs=document.getElementById("informacoes");
+            divs.innerHTML='';
 
+        pontos.forEach(ponto => {
+
+            const pino = L.marker([ponto.latitude, ponto.longitude]).bindPopup(`<b>${ponto.nome}</b>`);
+
+            const grupoCategoria = ponto.categoria_nome.toLowerCase();
+
+            if (todasCategorias[grupoCategoria]) {
+                pino.addTo(todasCategorias[grupoCategoria]);
+            } else {
+                console.warn(`categoria não encontrada ${grupoCategoria}`);
+            }
+
+      
+            const googleGPS = `https://www.google.com/maps/dir/?api=1&destination=${ponto.latitude},${ponto.longitude}`;
+            const wazeGPS = `https://waze.com/ul?ll=${ponto.latitude},${ponto.longitude}&navigate=yes`;
+
+            
+            divs.innerHTML+=`
+             <div class="ponto-card">
+                <div class="introducao">
+                    <img src="${ponto.imagem}">
+                    <div>
+                    <h3>${ponto.nome}</h3>
+                    <p><strong>Endereço : </strong>${ponto.endereco}</p>
+                    <p>${ponto.descricao}</p>
+                    </div>
+                </div>
+                <div class="links">
+                    <a href="${googleGPS}"  target="_blank" ><button id="maps">Abrir no Google Maps</button></a>
+                    <a href="${wazeGPS}"  target="_blank" ><button id="waze">Abrir no Waze</button></a>
+                </div>
+             </div>
+            `
+        })
+    }
+    catch (erro) {
+        console.error("Erro ao carregar os dados do mapa:", erro);
+    }
+}
 
 const botoes = document.querySelectorAll('.btn-filtro');
-
-
 botoes.forEach(function (botao) {
     botao.addEventListener('click', function () {
-
 
         botoes.forEach(b => b.classList.remove('ativo'));
 
         this.classList.add('ativo');
 
-
         for (var key in todasCategorias) {
             meuMapa.removeLayer(todasCategorias[key]);
         }
 
-
         var categoriaClicada = this.getAttribute('data-categoria');
-
 
         todasCategorias[categoriaClicada].addTo(meuMapa);
     });
 });
+carregarPontos();
