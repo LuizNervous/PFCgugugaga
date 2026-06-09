264a24bf0eabdfa32e60e0020515ecec1e4dc32f
GabrielBTostes1 <20251ac008007@estudantes.ifpr.edu.br>
2026-06-09 14:34:45 -0300

    ajeitando o home

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const botaoMenu=document.getElementById("botaoMenu");
+const fechar=document.getElementById("closeMenu");
+
 botaoMenu.addEventListener("click", ()=>{
   const divMenu=document.querySelector(".escondido");
   divMenu.classList.toggle('aberto')
+
 })
+fechar.addEventListener("click",()=>{
+const divMenu=document.querySelector(".escondido");
+  divMenu.classList.remove('aberto')
+});
