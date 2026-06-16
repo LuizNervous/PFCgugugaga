c9bfbd13f84573aa85b634019464cfaa06d433a6
GabrielBTostes1 <20251ac008007@estudantes.ifpr.edu.br>
2026-06-16 14:36:49 -0300

    fazendo menu

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ fechar.addEventListener("click",()=>{
 const divMenu=document.querySelector(".escondido");
   divMenu.classList.remove('aberto')
 });
+
+const botaoEscuro=document.getElementById("botaoEscuro");
+botaoEscuro.addEventListener("click",()=>{
+  document.body.classList.add("Escuro");
+})
+
+const botaoClaro=document.getElementById("botaoClaro");
+botaoClaro.addEventListener("click",()=>{
+  document.body.classList.remove("Escuro");
+})
